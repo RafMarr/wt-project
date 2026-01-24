7b24bbd0823811f6b6654bef238f4bd1db426fd8
Riccardo Traini <75073417+RicaBiz@users.noreply.github.com>
2026-01-24 12:41:46 +0100

    aggiunta schermata iniziale preview
    aggiunta schermata iniziale senza le immagini nel carosello

---
+++ preview.php
@@ -0,0 +1,10 @@
+<?php
+require_once("bootstrap.php");
+
+$templateParams["titolo"] = "Campus+";
+
+$templateParams["no-nav"] = "";
+$templateParams["nome"] = "benvenuto.php";
+require("template/base.php");
+
+?>
---
+++ template/benvenuto.php
@@ -0,0 +1,36 @@
+<div class="container-fluid text-center">
+    <h2>Benvenuto in Campus+!</h2>
+    <p>Il sito web per gli studenti dell'Universita' di Bologna</p>
+    <!-- Carosello, da intervenire poi con AJAX per le risorse -->
+    <div id="carosello" class="carousel slide">
+    <div class="carousel-indicators">
+        <button type="button" data-bs-target="#carosello" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+        <button type="button" data-bs-target="#carosello" data-bs-slide-to="1" aria-label="Slide 2"></button>
+        <button type="button" data-bs-target="#carosello" data-bs-slide-to="2" aria-label="Slide 3"></button>
+    </div>
+    <div class="carousel-inner">
+        <div class="carousel-item active">
+        <img src="..." class="d-block w-100" alt="">
+        </div>
+        <div class="carousel-item">
+        <img src="..." class="d-block w-100" alt="">
+        </div>
+        <div class="carousel-item">
+        <img src="..." class="d-block w-100" alt="">
+        </div>
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#carosello" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carosello" data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+    </button>
+    </div>
+    <p>Accedi o registrati per usufruire dei servizi creati appositamente per te!</p>
+    <div class="d-flex flex-column align-items-center gap-2">
+        <button type="button" class="btn btn-primary">Accedi</button>
+        <button type="button" class="btn btn-primary">Registrati</button>
+    </div>
+</div>
