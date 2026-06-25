553a4ae4563e945b84034eb2089b444acb6998ba
Raffaele Francesco Marrazzo <raffaele.marrazzo@studio.unibo.it>
2026-06-25 20:04:18 +0200

    Improved documentation in functions.php file

--- utils/functions.php
+++ utils/functions.php
@@ -14,7 +14,7 @@ function registerLoggedUser($idutente){
  * * if `day` is the current date, `start_time` must be at least the current time;
  * * `start_time` and `end_time` must be between `HIPPODROME_OPENING_TIME` and `HIPPODROME_WEEKDAYS_CLOSING_TIME` if the week day of `day` is between Monday and Friday; otherwise they must be between `HIPPODROME_OPENING_TIME` and `HIPPODROME_WEEKEND_CLOSING_TIME`;
  * * `end_time` must be at least `start_time`
- * @return `true` is all the parameters are valid, `false` otherwise
+ * @return bool `true` is all the parameters are valid, `false` otherwise
  */
 function are_pony_parameters_valid(string $day, string $start_time, string $end_time) : bool {
     $current_datetime = date("Y-m-d H:i");
