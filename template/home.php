0445e17f024ff177760b256bbee24bddae87b626
Riccardo Traini <75073417+RicaBiz@users.noreply.github.com>
2026-07-06 16:30:41 +0200

    Aggiunto link a report.php

--- template/home.php
+++ template/home.php
@@ -54,7 +54,7 @@
     </div>
     <div class="col-6 row justify-content-center mb-3 mx-0 p-0">
         <div class="col-10 shadow border-solid border-3 border-mode-text rounded-3 p-2">
-            <a href="segnalazioni.php" class="nav-link">
+            <a href="report.php" class="nav-link">
                 <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="50" fill="currentColor" class="bi bi-bus-front" viewBox="0 0 16 16">
                 <path d="M7.938 2.016A.13.13 0 0 1 8.002 2a.13.13 0 0 1 .063.016.15.15 0 0 1 .054.057l6.857 11.667c.036.06.035.124.002.183a.2.2 0 0 1-.054.06.1.1 0 0 1-.066.017H1.146a.1.1 0 0 1-.066-.017.2.2 0 0 1-.054-.06.18.18 0 0 1 .002-.183L7.884 2.073a.15.15 0 0 1 .054-.057m1.044-.45a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767z"/>
                 <path d="M7.002 12a1 1 0 1 1 2 0 1 1 0 0 1-2 0M7.1 5.995a.905.905 0 1 1 1.8 0l-.35 3.507a.552.552 0 0 1-1.1 0z"/>
