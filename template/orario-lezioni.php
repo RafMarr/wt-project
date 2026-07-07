01a4edd16e83e0758d9a5dcd765af33a23f3666d
Riccardo Traini <75073417+RicaBiz@users.noreply.github.com>
2026-07-07 18:41:40 +0200

    Aggiustato stile dell'orario della lezione

--- template/orario-lezioni.php
+++ template/orario-lezioni.php
@@ -32,14 +32,17 @@
         <?php foreach ($templateParams["lessons"] as $lesson): ?>
             <div class="row justify-content-center border-mode-text border-solid rounded mode-gray lesson-card-md col-10 p-2">
                 <div class="row justify-content-between justify-content-md-start border-b border-mode-text border-md-0 col-12">
-                    <div class="row col-3 align-items-center row-gap-2 m-0">
-                        <div class="d-none d-md-inline col-2 p-0" aria-hidden="true">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-clock-fill" viewBox="0 0 16 16">
+                    <div class="row col-3 align-items-center justify-content-start row-gap-2 m-0">
+                        <div class="row col-3 align-items-center text-md-start m-0">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-clock-fill d-none d-md-inline col-md-2 p-0" viewBox="0 0 16 16" aria-label="Orario:">
                                 <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M8 3.5a.5.5 0 0 0-1 0V9a.5.5 0 0 0 .252.434l3.5 2a.5.5 0 0 0 .496-.868L8 8.71z"/>
                             </svg>
+                            <div class="row col-12 col-md-10 p-0 m-0">
+                                <p class="col-12 col-md-5 p-0 m-0"><?php echo substr($lesson["StartTime"], 5); ?></p>
+                                <span class="d-none d-md-inline col-md-2 p-0">-</span>
+                                <p class="col-12 col-md-5 p-0 m-0"><?php echo substr($lesson["EndTime"], 5); ?></p>
+                            </div>
                         </div>
-                        <p class="col-12 col-md-5 p-0 m-0"><?php echo substr($lesson["StartTime"], 5); ?></p>
-                        <p class="col-12 col-md-5 p-0 m-0"><?php echo substr($lesson["EndTime"], 5); ?></p>
                     </div>
                     <div class="col-9 d-flex align-items-center justify-content-center justify-content-md-start">
                         <h3 class="fs-4 my-1"><a class="mode-text" href="#"><?php echo $lesson["CourseName"]; ?></a></h3>
