111aa6593fbfddd1a76cb0126f01383ce43f3ad1
Riccardo Traini <75073417+RicaBiz@users.noreply.github.com>
2026-07-07 10:36:11 +0200

    Aggiunto aria-hidden all'icona nell'header

--- template/base.php
+++ template/base.php
@@ -16,7 +16,7 @@
     <a href="#main-content" class="visually-hidden">Vai al contenuto principale</a>
     <header class="text-center theme-bg-text py-2">
         <a class="d-flex justify-content-center align-items-center nav-link" href="index.php">
-            <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor" class="bi bi-mortarboard-fill" viewBox="0 0 16 16">
+            <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor" class="bi bi-mortarboard-fill" viewBox="0 0 16 16" aria-hidden="true">
                 <path d="M8.211 2.047a.5.5 0 0 0-.422 0l-7.5 3.5a.5.5 0 0 0 .025.917l7.5 3a.5.5 0 0 0 .372 0L14 7.14V13a1 1 0 0 0-1 1v2h3v-2a1 1 0 0 0-1-1V6.739l.686-.275a.5.5 0 0 0 .025-.917z"/>
                 <path d="M4.176 9.032a.5.5 0 0 0-.656.327l-.5 1.7a.5.5 0 0 0 .294.605l4.5 1.8a.5.5 0 0 0 .372 0l4.5-1.8a.5.5 0 0 0 .294-.605l-.5-1.7a.5.5 0 0 0-.656-.327L8 10.466z"/>
             </svg>
