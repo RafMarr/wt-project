8b56f42201f6c357b2290d939720a265f170a3b5
Raffaele Francesco Marrazzo <raffaele.marrazzo@studio.unibo.it>
2026-07-13 18:50:29 +0200

    Moved modals and offcanvas at the bottom of the pony template file

--- template/pony.php
+++ template/pony.php
@@ -37,6 +37,44 @@ if (isset($templateParams['booking-successful'])) {
     </div>
 <?php endif; ?>
 
+<div class="px-3 text-center pt-5">
+    <header class="d-flex justify-content-center align-items-center gap-3">
+        <h2>Noleggia un pony</h2>
+        <button type="button" class="btn mode-text p-0 pb-2" data-bs-toggle="modal" data-bs-target="#hippodromeModal" aria-label="Maggiori informazioni sull'ippodromo">
+            <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-info-circle" viewBox="0 0 16 16">
+                <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
+                <path d="m8.93 6.588-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0"/>
+            </svg>
+        </button>
+    </header>
+    <p>Per verificare la disponibilità dei pony ed effettuare una prenotazione, inserire i seguenti valori:</p>
+</div>
+<button type="button" class="btn border-top-0 border-start-0 border-end-0 py-1 px-2 border border-2 mode-container mode-text position-absolute top-0 end-0" data-bs-toggle="offcanvas" data-bs-target="#filtersMenu" aria-controls="filtersMenu">
+    Filtra ricerca
+    <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-filter" viewBox="0 0 16 16">
+        <path d="M6 10.5a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 0 1h-3a.5.5 0 0 1-.5-.5m-2-3a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5m-2-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5"/>
+    </svg>
+</button>
+<section id="booking-params" class="row mx-0 justify-content-center column-gap-md-2 p-0 mb-5">
+    <h3 class="visually-hidden">Parametri di ricerca</h3>
+    <div class="d-md-flex flex-column gap-2 justify-content-start col-10 col-md-3 col-xl-2 mb-3 mb-md-0 text-start">
+        <label for="day" class="form-label m-md-0">Giorno</label>
+        <input type="date" min="<?php echo $min_date ?>" class="form-control mode-input-border-color is-invalid" name="day" id="day" aria-describedby="day-feedback" />
+        <div id="day-feedback" class="invalid-feedback m-md-0"></div>
+    </div>
+    <div class="d-md-flex flex-column gap-2 justify-content-start col-10 col-md-3 col-xl-2 mb-3 mb-md-0 text-start">
+        <label for="start-time" class="form-label m-md-0">Ora inizio</label>
+        <input type="time" min="<?php echo HIPPODROME_OPENING_TIME ?>" max="<?php echo HIPPODROME_WEEKDAYS_CLOSING_TIME ?>" class="form-control mode-input-border-color is-invalid" name="start-time" id="start-time" aria-describedby="start-time-feedback" />
+        <div id="start-time-feedback" class="invalid-feedback m-md-0"></div>
+    </div>
+    <div class="d-md-flex flex-column gap-2 justify-content-start col-10 col-md-3 col-xl-2 text-start">
+        <label for="end-time" class="form-label m-md-0">Ora fine</label>
+        <input type="time" min="<?php echo HIPPODROME_OPENING_TIME ?>" max="<?php echo HIPPODROME_WEEKDAYS_CLOSING_TIME ?>" class="form-control mode-input-border-color is-invalid" name="end-time" id="end-time" aria-describedby="end-time-feedback" />
+        <div id="end-time-feedback" class="invalid-feedback m-md-0"></div>
+    </div>
+</section>
+<section id="available-ponies" class="text-center col-10 row row-cols-1 row-cols-md-2 row-cols-lg-3 g-3 mx-auto">
+</section>
 <aside class="offcanvas offcanvas-start mode-modal p-2 pb-3" tabindex="-1" id="filtersMenu" aria-labelledby="filtersTitle">
     <div class="offcanvas-header">
         <h2 class="offcanvas-title text-center mode-text" id="filtersTitle">Filtra ricerca</h2>
@@ -104,41 +142,3 @@ if (isset($templateParams['booking-successful'])) {
         </div>
     </div>
 </div>
-<div class="px-3 text-center pt-5">
-    <header class="d-flex justify-content-center align-items-center gap-3">
-        <h2>Noleggia un pony</h2>
-        <button type="button" class="btn mode-text p-0 pb-2" data-bs-toggle="modal" data-bs-target="#hippodromeModal" aria-label="Maggiori informazioni sull'ippodromo">
-            <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-info-circle" viewBox="0 0 16 16">
-                <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
-                <path d="m8.93 6.588-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0"/>
-            </svg>
-        </button>
-    </header>
-    <p>Per verificare la disponibilità dei pony ed effettuare una prenotazione, inserire i seguenti valori:</p>
-</div>
-<button type="button" class="btn border-top-0 border-start-0 border-end-0 py-1 px-2 border border-2 mode-container mode-text position-absolute top-0 end-0" data-bs-toggle="offcanvas" data-bs-target="#filtersMenu" aria-controls="filtersMenu">
-    Filtra ricerca
-    <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-filter" viewBox="0 0 16 16">
-        <path d="M6 10.5a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 0 1h-3a.5.5 0 0 1-.5-.5m-2-3a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5m-2-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5"/>
-    </svg>
-</button>
-<section id="booking-params" class="row mx-0 justify-content-center column-gap-md-2 p-0 mb-5">
-    <h3 class="visually-hidden">Parametri di ricerca</h3>
-    <div class="d-md-flex flex-column gap-2 justify-content-start col-10 col-md-3 col-xl-2 mb-3 mb-md-0 text-start">
-        <label for="day" class="form-label m-md-0">Giorno</label>
-        <input type="date" min="<?php echo $min_date ?>" class="form-control mode-input-border-color is-invalid" name="day" id="day" aria-describedby="day-feedback" />
-        <div id="day-feedback" class="invalid-feedback m-md-0"></div>
-    </div>
-    <div class="d-md-flex flex-column gap-2 justify-content-start col-10 col-md-3 col-xl-2 mb-3 mb-md-0 text-start">
-        <label for="start-time" class="form-label m-md-0">Ora inizio</label>
-        <input type="time" min="<?php echo HIPPODROME_OPENING_TIME ?>" max="<?php echo HIPPODROME_WEEKDAYS_CLOSING_TIME ?>" class="form-control mode-input-border-color is-invalid" name="start-time" id="start-time" aria-describedby="start-time-feedback" />
-        <div id="start-time-feedback" class="invalid-feedback m-md-0"></div>
-    </div>
-    <div class="d-md-flex flex-column gap-2 justify-content-start col-10 col-md-3 col-xl-2 text-start">
-        <label for="end-time" class="form-label m-md-0">Ora fine</label>
-        <input type="time" min="<?php echo HIPPODROME_OPENING_TIME ?>" max="<?php echo HIPPODROME_WEEKDAYS_CLOSING_TIME ?>" class="form-control mode-input-border-color is-invalid" name="end-time" id="end-time" aria-describedby="end-time-feedback" />
-        <div id="end-time-feedback" class="invalid-feedback m-md-0"></div>
-    </div>
-</section>
-<section id="available-ponies" class="text-center col-10 row row-cols-1 row-cols-md-2 row-cols-lg-3 g-3 mx-auto">
-</section>
