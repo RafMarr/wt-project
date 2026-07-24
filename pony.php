5443ae85688534d53ee47aeef1e60d296bcdc46e
Raffaele Francesco Marrazzo <raffaele.marrazzo@studio.unibo.it>
2026-07-24 18:08:26 +0200

    Included polyfill for Temporal javascript library

--- pony.php
+++ pony.php
@@ -7,7 +7,7 @@ if(!isUserLoggedIn()) {
 
 $templateParams["titolo"] = "Campus+ - Noleggia un pony";
 $templateParams["nome"] = "template/pony.php";
-$templateParams["js"] = array("js/pony.js");
+$templateParams["js"] = array("js/pony.js", "https://cdn.jsdelivr.net/npm/@js-temporal/polyfill/dist/index.umd.js");
 
 if (isset($_GET['booking-successful']) && (($_GET['booking-successful'] == "true") || ($_GET['booking-successful'] == "false"))) {
     $templateParams['booking-successful'] = $_GET['booking-successful'] == "true" ? true : false;
